4524227221b710b058ece7d747cd27239af14009
Marcos Curtidor <3048652220116@ingenieria.usac.edu.gt>
2022-04-27 22:45:40 -0600

    Arreglo logs Mongo

--- rabbit-subscriber/Mongo/saveLogs.go
+++ rabbit-subscriber/Mongo/saveLogs.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -18,15 +19,53 @@ const (
 
 // JSON PARA RECIBIR DE RABBIT
 type Logs struct {
-	Game_id   int32  `json:"game_id"`
-	Players   int32  `json:"players"`
-	Game_name string `json:"game_name"`
-	Winner    int32  `json:"winner"`
-	Queue     string `json:"queue"`
+	Request_number int32  `json:"request_number"`
+	Game_id        int32  `json:"game_id"`
+	Players        int32  `json:"players"`
+	Game_name      string `json:"game_name"`
+	Winner         int32  `json:"winner"`
+	Queue          string `json:"queue"`
+}
+
+func CountLogs() int32 {
+	host := os.Getenv("HOSTIP_MONGO")
+
+	if len(host) == 0 {
+		host = "localhost"
+	}
+
+	// OPENING CONNECTION TO MONGODB
+	clientOpts := options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@%s:%d", user, password, host, port))
+	client, err := mongo.Connect(context.TODO(), clientOpts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// VERIFYING THE CONNECTION
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Conexion exitosa")
+
+	// CONNECTION TO DATABASE AND COLLECTION
+	collection := client.Database("so1-practica2").Collection("logs")
+
+	// GET OPERATIONS
+	data, err := collection.CountDocuments(context.TODO(), bson.D{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return int32(data)
 }
 
 // GUARDANDO LOGS EN MONGODB
 func SaveLogs(logsData Logs) {
+	logsData.Request_number = CountLogs() + 1
+
 	host := os.Getenv("HOSTIP_MONGO")
 
 	if len(host) == 0 {
